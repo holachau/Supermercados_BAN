4b5ac4dff1003a00f53b815fd9c524b67001a48a
Nico <nicolas.benacar@comunidad.ub.edu.ar>
2018-10-29 15:05:31 -0300

    Cambios y mejoras

--- modules/action_8.py
+++ modules/action_8.py
@@ -4,22 +4,22 @@ from productos import findByCodigo, updateProducto
 
 #El usuario solicita poder hacer envios a domicilio. Pedir los datos de un cliente para hacer envío a domicilio.
 def envio():
-    prod = input("¿Qué producto desea? ")
+    prod = input("Ingresar código del producto: ")
     verif_compra = findByCodigo(prod)
-    while verif_compra == None:
-        prod = input("¿Qué producto desea? ")
-        verif_compra = findByCodigo(prod)
-    cant = int(input("¿Cuántos va a llevar? "))
-    if verif_compra["stock"] < cant:
-        print("Error, no hay tantos")
+    if verif_compra == None:
+        print("El producto no existe")
     else:
-        precio_tot = cant * verif_compra["precioUnitario"]
-        print(precio_tot)
-        continuar = input("¿Va a llevarlo? ")
-        if continuar == "no" or continuar == "No":
-            print("Ok.")
-        elif continuar == "si" or continuar == "Si":
-            print("Por favor, verífiquenos su información")
-            ubicacion = input("Domicilio: ")
-            tel = input("Teléfono: ")
-            nombre = input("Nombre: ")
+        cant = int(input("¿Cuántos va a llevar? "))
+        if verif_compra["stock"] < cant:
+            print("Error, no hay tantos")
+        else:
+            precio_tot = cant * verif_compra["precioUnitario"]
+            print("El precio total es: ", precio_tot)
+            continuar = input("¿Va a llevarlo? ")
+            if continuar == "no" or continuar == "No":
+                print("Ok.")
+            elif continuar == "si" or continuar == "Si":
+                print("Por favor, verífiquenos su información")
+                ubicacion = input("Domicilio: ")
+                tel = input("Teléfono: ")
+                nombre = input("Nombre: ")
