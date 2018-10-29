fa78a85dc72ec14067c77306015781cb883fbea3
Nico <nicolas.benacar@comunidad.ub.edu.ar>
2018-10-29 13:12:35 -0300

    Acción 4 hecha

--- modules/action_4.py
+++ modules/action_4.py
@@ -1,1 +1,14 @@
-#test 
+import sys
+sys.path.insert(0, './lib')
+from productos import findByCodigo, updateProducto
+
+
+def newStock():
+    n = input("¿Qué producto busca? ")
+    m = int(input("¿Cuántos se vendieron? "))
+    analizar_stock = findByCodigo(n)
+    if analizar_stock == False:
+        return "El objeto no existe"
+    analizar_stock["stock"] -= m
+    updateProducto(analizar_stock)
+    print("Lista actualizada.")
