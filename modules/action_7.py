d97d3e0b018af54ff7d6b73ded7d88eebda77b21
Nico <nicolas.benacar@comunidad.ub.edu.ar>
2018-10-30 19:28:11 -0300

    Acción 7

---
+++ modules/action_7.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.append('./lib')
+from productos import findByCodigo, updateProducto
+
+
+
+#El usuario solicita poder reponer un producto cuando el stock este por debajo de un mínimo requerido.
+
+def reponer_Stock ():
+    n = input("¿Qué producto busca? ")
+    analizar_stock = findByCodigo(n)
+    if analizar_stock == None:
+        return "El producto no existe."
+    else:
+        print("Cantidad total de stock: ", analizar_stock["stock"])
+        preg = input("¿Desea reponer el stock? ")
+        if preg == "si" or preg == "Si":
+            reponer = int(input("¿Cuántos quiere agregar?"))
+            analizar_stock["stock"] = analizar_stock["stock"] + reponer
+            updateProducto(analizar_stock)
+            print(analizar_stock["stock"])
+        elif preg == "no" or preg == "No":
+            return "Ok."
