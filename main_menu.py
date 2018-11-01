32cf97e8a478561bd101f1719f476ddcfacc972c
Agustin Ramos <you@example.com>
2018-10-31 21:07:36 -0300

    change option 4

--- main_menu.py
+++ main_menu.py
@@ -31,7 +31,7 @@ while True:
     print('// (1)  Agregar un nuevo producto                                        //')
     print('// (2)  Eliminar un producto introduciendo su codigo                     //')
     print('// (3)  Listar los productos en stock                                    //')
-    print('// (4)  Actualiar stock de un producto                                   //')
+    print('// (4)  Actualiar stock de un producto tras una venta                    //')
     print('// (5)  Actualizar el precio unitario de un producto porcentualmente     //')
     print('// (6)  Cantidad de stock de un producto                                 //')
     print('// (7)  Introducir stock minimo de un producto                           //')
